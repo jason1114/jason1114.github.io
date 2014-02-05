1dc8287e0824cc9982998a4f458a917c5feeb839
jason <znlswd@gmail.com>
2014-02-05 19:29:08 +0800

    release

--- js/new_post.js
+++ js/new_post.js
@@ -1,4 +1,6 @@
-blogListURL = "http://bolgeditor.sturgeon.mopaas.com/"
+blogListURL = 
+"http://localhost/blog-editor/"
+//"http://bolgeditor.sturgeon.mopaas.com/"
 $windowClass = $("<link/>").attr("rel","stylesheet").attr("href","/css/themes/umbra/style.css")
 $windowScript = $("<script/>").attr("type","text/javascript").attr("src","/js/jWindow.js")
 $("head").append($windowClass).append($windowScript)
@@ -29,4 +31,35 @@ $(function(){
 			//is_editor_showed = true;
 		}
 	})
+	var onmessage = function(e) {
+       var data = e.data
+       $(".post").html(e.data['html'])
+       $($("#intro h2")[0]).text(data['title'])
+       $("title").text()
+       $("#post_date").text(data['date'])
+       $("#tags").html('')
+       for(var i=0;i<data['tags'].length;i++){
+       		$span = $("<span/>")
+       		$a = $("<a/>").attr("href","/articles/"+data['tags'][i]).text(data['tags'][i])
+       		$span.append($a)
+       		$("#tags").append($span)
+       		$("#tags").append("\n")
+       }
+       $(".highlight").each(function(index,element){
+
+       		$(this).load("http://springlin.herokuapp.com/",
+       			{lang:$(this).find("pre code").attr("class"),code:$(this).find("pre code").text()},
+       			function(){
+       				$(this).find(".highlight").unwrap()
+       			})
+
+       })
+
+    };
+    //监听postMessage消息事件
+    if (typeof window.addEventListener != 'undefined') {
+      window.addEventListener('message', onmessage, false);
+    } else if (typeof window.attachEvent != 'undefined') {
+      window.attachEvent('onmessage', onmessage);
+    }
 })
